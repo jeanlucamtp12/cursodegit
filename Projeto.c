281a0a0e21c0dfa3e64867b253fde72b1a0b4771
Jeanluca <jeaanlucamartins@gmail.com>
2019-06-11 15:02:42 -0300

    Adicionando funcao de subtracao corrigida

--- Projeto.c
+++ Projeto.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
 
+int subtracao(int a , int b)
+{
+	
+	int sub_num = a - b;
+	
+	return sub_num;
+	
+}
+
+int soma(int a , int b)
+{
+	int soma_num = a + b;
+	
+	return soma_num;
+}
+
+int main ()
+{
+	
+	printf ("A soma dos numero %d\n",soma(3,4));
+	printf ("A subtracao dos numeros %d\n",subtracao(5,2));
+	
+	return 0;
+	
+}
